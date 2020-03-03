c54d48a7cab0261e8a307090a9c1f9a86ef02260
xrotwang <xrotwang@googlemail.com>
2020-03-03 21:20:46 +0100

    almost there

--- cldfbench_dryerorder.py
+++ cldfbench_dryerorder.py
@@ -1,62 +1,32 @@
-import re
 import pathlib
+import collections
 
-from pyglottolog import Glottolog
-from pyglottolog.fts import search
+from clldutils.coordinates import Coordinates
+from clldutils.misc import slug
+from clldutils.text import split_text_with_context
 from pybtex.database import parse_string
 
 from cldfbench import Dataset as BaseDataset, CLDFSpec
 
 HEADER = [
-    'language', 'iso', 'genus', 'family', 'lat', 'lon', 'val',
-    't3',
-    't4',
-    't5',
-    't6',
-    't7',
-    't8',
-    't9',
-    't10',
-    't13',
+    'language', 'iso', 'genus', 'family', 'lat', 'lon',
+    'Table1',
+    'Table3',
+    'Table4',
+    'Table5',
+    'Table6',
+    'Table7',
+    'Table8',
+    'Table9',
+    'Table10',
+    'Table13',
 ]
 # Baule,tul,Kwa,Niger-Congo,7dN,5dW,N-A-Num-Dem,,,,,,,,,
 
 
-def iter_entries(lines):
-    agg = []
-    for line in lines:
-        line = line.strip()
-        if line:
-            if re.match('[A-Z] [A-Z]', line):
-                if agg:
-                    yield ' '.join(agg)
-                agg = [line]
-            else:
-                agg.append(line)
-    if agg:
-        yield ' '.join(agg)
-
-
-def parse_authors(s):
-    if s.endswith(' .'):
-        s = s[:-2].strip()
-    return [
-        re.sub('(^| )(?P<initial>[A-Z]) (?P<rem>[A-Z]+)', lambda m: ' {0}{1}'.format(m.group('initial'), m.group('rem').lower()), ss).strip()
-        for ss in s.split(' AND ')]
-
-
-def parse_entry(e):
-    yearp = re.compile('\s+(?P<refyear>(?P<year>[0-9]{4})[abcde]?)(\s+\[[0-9\-]+\])?\.\s*')
-    year = yearp.search(e)
-    assert year
-    author = e[:year.start()].strip()
-    title = e[year.end():].split('.')[0].strip()
-    authors = parse_authors(author)
-    return (
-        authors,
-        year.group('year'),
-        title,
-        '{0} {1}'.format(' and '.join([a.split(',')[0].strip() for a in authors]), year.group('refyear')))
+def clean_coords(c):
+    c = c.replace('?', '')
+    return {'21d43dN': '21d43N'}.get(c, c)
 
 
 class Dataset(BaseDataset):
@@ -69,17 +39,79 @@ class Dataset(BaseDataset):
     def cmd_download(self, args):
         # https://muse.jhu.edu/article/712564/file/supp02.csv
         # https://muse.jhu.edu/article/712564/file/supp01.pdf
-
-        bd = parse_string(self.raw_dir.read('sources.bib'), 'bibtex')
-        keys = {e.fields['key']: k for k, e in bd.entries.items()}
-
-        langs = {r[0]: dict(zip(HEADER, r)) for r in self.raw_dir.read_csv('Dryer_DNAnTable.txt', encoding='macroman')}
-        langs_with_val = {r for r in langs if langs[r]['val']}
-
+        pass
 
     def cmd_makecldf(self, args):
         args.writer.cldf.add_component('ParameterTable')
-        args.writer.cldf.add_component('LanguageTable')
+        args.writer.cldf.add_component('LanguageTable', 'Genus', 'Family')
         args.writer.cldf.add_component('CodeTable')
+        args.writer.cldf.add_sources(parse_string(self.raw_dir.read('sources.bib'), 'bibtex'))
 
-        #Language_Name,ISO_code,Genus,Family,Latitude,Longitude,Order_of_Dem_Num_A_N,Table_3,Table_4,Table_5,Table_6,Table_7,Table_8,Table_9,Table_10,Table_13_and_13B
+        for row in self.raw_dir.read_csv('parameters.csv', dicts=True):
+            args.writer.objects['ParameterTable'].append(dict(
+                ID=row['ID'],
+                Name=row['ID'],
+                Description=row['Description'],
+            ))
+
+        codes = collections.defaultdict(dict)
+        for row in self.raw_dir.read_csv('codes.csv', dicts=True):
+            args.writer.objects['CodeTable'].append(dict(
+                ID=row['ID'],
+                Name=row['Name'],
+                Description=row['Description'],
+                Parameter_ID=row['Parameter_ID'],
+                # FIXME: add abbr!
+            ))
+            codes[row['Parameter_ID']][row['Name']] = row['ID']
+
+        glottocodes = {r['name']: r['glottocode'] for r in self.etc_dir.read_csv('languages.csv', dicts=True)}
+        sources = {r['language']: r for r in self.raw_dir.read_csv('sources.csv', dicts=True)}
+        vid = 0
+        gl_by_iso = {}
+        gl_by_id = {}
+        for l in args.glottolog.api.languoids():
+            if l.iso:
+                gl_by_iso[l.iso] = l
+            gl_by_id[l.id] = l
+
+        for row in self.raw_dir.read_csv('Dryer_DNAnTable.txt', encoding='macroman'):
+            row = dict(zip(HEADER, row))
+            coords = Coordinates(clean_coords(row['lat']), clean_coords(row['lon']))
+            iso = row['iso'] if row['iso'] != '--' else None
+            lid = slug(row['language'] + (row['iso'] or ''))
+            if row['language'] in glottocodes:
+                glang = gl_by_id[glottocodes[row['language']]]
+            else:
+                glang = gl_by_iso.get(row['iso'])
+            if not glang:
+                print(row['language'], row['iso'])
+            args.writer.objects['LanguageTable'].append(dict(
+                ID=lid,
+                Name=row['language'],
+                Latitude=coords.latitude,
+                Longitude=coords.longitude,
+                ISO639P3code=iso,
+                Glottocode=glang.id if glang else None,
+                Genus=row['genus'],
+                Family=row['family'],
+            ))
+            for p in args.writer.objects['ParameterTable']:
+                source, comment = [], None
+                if p['ID'] == 'Table1' and row['language'] in sources:
+                    source = split_text_with_context(
+                        sources[row['language']]['sources'], separators=';', brackets={'[': ']'})
+                    comment = sources[row['language']]['comment']
+                if row[p['ID']]:
+                    vid += 1
+                    args.writer.objects['ValueTable'].append(dict(
+                        ID=str(vid),
+                        Language_ID=lid,
+                        Parameter_ID=p['ID'],
+                        Value=row[p['ID']],
+                        Code_ID=codes[p['ID']][row[p['ID']]],
+                        Source=source,
+                        Comment=comment,
+                    ))
+
+            #langs_with_val = {r for r in langs if langs[r]['Table1']}
