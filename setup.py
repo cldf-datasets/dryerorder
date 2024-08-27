f46a6589e719059edcd1ef9dfbf64843ce4eee7e
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:03:06 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_dryerorder',
-    py_modules=['cldfbench_dryerorder'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'dryerorder=cldfbench_dryerorder:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
